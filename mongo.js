7e64ea4a8b349e42f9ee93015562466b13f4f171
HassanKhan123 <hk2602809@gmail.com>
2020-08-21 07:46:05 +0500

    create a simple backend and connect to the database

---
+++ mongo.js
@@ -0,0 +1,15 @@
+const MongoClient = require('mongodb').MongoClient;
+
+const url = 'mongodb+srv://hassan123:hassan123@cluster0.mgicu.mongodb.net/products?retryWrites=true&w=majority'
+
+const createProduct = async(req,res,next) => {
+
+}
+
+const getProducts = async(req,res,next) => {
+    
+}
+
+module.exports = {
+    createProduct,getProducts
+}
